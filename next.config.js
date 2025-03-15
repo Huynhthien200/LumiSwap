18733ae67ab8663149dfffa97c543e50eba73392
Huynhthien200 <128266121+Huynhthien200@users.noreply.github.com>
2025-03-15 17:14:39 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   env: {
-    NEXT_PUBLIC_RPC_URL: process.env.NEXT_PUBLIC_RPC_URL,
-    NEXT_PUBLIC_CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+    NEXT_PUBLIC_RPC_URL: "https://dream-rpc.somnia.network/",
+    NEXT_PUBLIC_CHAIN_ID: "50312",
+    NEXT_PUBLIC_BLOCK_EXPLORER: "https://shannon-explorer.somnia.network/",
+    NEXT_PUBLIC_TOKEN_SYMBOL: "STT",
+  },
+  images: {
+    domains: ["raw.githubusercontent.com", "somnia.network"], // Để hiển thị logo token nếu cần
   },
 };
+
+module.exports = nextConfig;
