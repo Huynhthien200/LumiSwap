f82685a5b7f387afbe8b13a73ad50601399cd1c2
Huynhthien200 <128266121+Huynhthien200@users.noreply.github.com>
2025-03-15 16:39:09 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_RPC_URL: process.env.NEXT_PUBLIC_RPC_URL,
+    NEXT_PUBLIC_CONTRACT_ADDRESS: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+  },
+};
