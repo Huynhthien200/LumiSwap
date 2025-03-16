89f675101db0e1b5bf513f1b524461a7346d0441
Huynhthien200 <128266121+Huynhthien200@users.noreply.github.com>
2025-03-16 17:53:03 +0700

    Delete components directory

--- components/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-  return (
-    <header className="bg-gray-900 text-white p-5 flex justify-between items-center shadow-md">
-      <h1 className="text-xl font-bold">Somnia Swap</h1>
-      <ConnectWallet />
-    </header>
-  );
-};
-export default Header;
